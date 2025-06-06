0c4a07d3d8e1f0a14697f304f416056d31765e9c
Priya Dharshini <vdharshini2211@gmail.com>
2025-06-06 13:16:07 +0530

    commit

--- login.js
+++ login.js
@@ -1,17 +1,23 @@
-const SUPABASE_URL = "https://dbwmdvkndczqruuzwxtd.supabase.co";
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRid21kdmtuZGN6cXJ1dXp3eHRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxOTQ1MzEsImV4cCI6MjA2NDc3MDUzMX0.U--eqvp3XE-L75Nf5qjCUve6-secciOHFDY_oTKaJFM';
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const SUPABASE_URL = "https://dbmvdknkdzcqrzuuxtxd.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRibXZka25rZHpjcnp1dXh0eGQiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTc0OTE5MzYyNiwiZXhwIjoyMDY0NzUxMjI2fQ.PV1jNkb_BHJZwV4TS6S_vn0mNMXFoMc3ceYcFVuPEEA";
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 async function login() {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const role = document.getElementById('role').value;
 
-    let { data, error } = await supabase.auth.signInWithPassword({ email, password });
+    let { data, error } = await client.auth.signInWithPassword({ email, password });
     if (error) return alert(error.message);
 
-    const { data: userData } = await supabase.from('users').select().eq('email', email).single();
-    if (userData.role !== role) return alert("Invalid role");
+    const { data: userData } = await client
+        .from('users')
+        .select()
+        .eq('email', email)
+        .single();
+
+    if (!userData || userData.role !== role) return alert("Invalid role");
 
     window.location.href = role === 'buyer' ? 'buyer.html' : 'seller.html';
 }
